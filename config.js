8d3eb4d435553fc5266f5d06b05a79bb7aa3ae92
amor <amor_zhang@icloud.com>
2016-04-27 09:37:19 +0800

    Create config.js

---
+++ config.js
@@ -0,0 +1,29 @@
+/**
+ *
+ * @Description 系统配置
+ * @Author Amor
+ * @Created 2016/04/26 11:48
+ * 技术只是解决问题的选择,而不是解决问题的根本...
+ * 我是Amor,为发骚而生!
+ *
+ */
+
+module.exports = {
+  
+    //邮件配置
+    email: {
+        /**
+         *
+         * 支持的服务:'1und1','AOL','DebugMail.io','DynectEmail','FastMail',
+         * 'GandiMail','Gmail','Godaddy','GodaddyAsia','GodaddyEurope','hot.ee',
+         * 'Hotmail','iCloud','mail.ee','Mail.ru','Mailgun','Mailjet','Mandrill',
+         * 'Naver','OpenMailBox','Postmark','QQ','QQex','SendCloud','SendGrid',
+         * 'SES','SES-US-EAST-1','SES-US-WEST-1','SES-EU-WEST-1','Sparkpost','Yahoo',
+         * 'Yandex','Zoho'
+         *
+         */
+        service: 'QQ',
+        user: '邮箱',
+        pass: '密码',
+    }
+}
